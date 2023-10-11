0372ef0c67e9edcf7ff4c17dcde6fab3420f7336
Bighanz98 <Antonhanz9@gmail.com>
2023-10-11 05:56:25 +0200

    Jag har skapat en inloggingsmetod som kommer upp när man startar programmet. I den har jag implementerat bankmenymetoden, så om allt går rätt till i inloggningsprocessen, kommer användaren vidare till menyn. Jag har även gjort klart utlogginsmetoden.

--- Individuellt projekt/Program.cs
+++ Individuellt projekt/Program.cs
@@ -1,64 +1,124 @@
-﻿namespace Individuellt_projekt
+﻿using System.Reflection.Metadata.Ecma335;
+
+namespace Individuellt_projekt
 {
     internal class Program
     {
+        static string[] userAccounts = { "Anton", "Adam", "Simon", "Petter", "Linus" };
+        static string[] userPinCodes = { "1111", "2222", "3333", "4444", "5555" };
+        static string[] userAccAndBalance = { "Sparkonto", "Semsterkonto", "Spelkonto", "Sparkonto", "konto"};
         static void Main(string[] args)
         {
-            
-            string[] userAccounts = { "Anton", "Adam", "Simon", "Petter", "Linus" };
-            
+
             Console.WriteLine("----------------------");
             Console.WriteLine("Välkommen till banken!");
             Console.WriteLine("----------------------");
 
-            BankMenu();
+            LogIn();
+
 
             Console.ReadKey();
         }
-        static void BankMenu()
-        {                                  
-            Console.WriteLine("1. Se dina konton och saldo ");
-            Console.WriteLine("2. Överföring mellan konton");
-            Console.WriteLine("3. Ta ut pengar ");
-            Console.WriteLine("4. Logga ut ");
-            Console.Write("Välj ett alternativ:  ");
-            int userChoice = Convert.ToInt32(Console.ReadLine());
+        static void LogIn()
+        {
+            bool loggedIn = false;
+            int maxLogAttempts = 3;
 
-            switch (userChoice)
+            while (!loggedIn && maxLogAttempts > 0)
             {
-                case 1:
-                    AccAndBalance();
-                    break;
-                case 2:
-                    TransferMoney();
-                    break;
-                case 3:
-                    WithdrawMoney();
-                    break;
-                case 4:
-                    LogOut();
-                    break;
-                default:
-                    break;
+                Console.Write("Logga in med din PIN-kod:");
+                string userPIN = Console.ReadLine();
 
+                for (int i = 0; i < userPinCodes.Length; i++)
+                {
+                    if (userPinCodes[i] == userPIN)
+                    {
+                        Console.Clear();
+                        Console.WriteLine($"Hej, {userAccounts[i]}!");                      
+                        loggedIn = true;
+                        BankMenu();
+                        break;                       
+                    }
+                }
+                if (!loggedIn) 
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine($"\nFelaktig PIN-kod! Försök igen, du har {maxLogAttempts} försök kvar");
+                    Console.ResetColor();
+                    maxLogAttempts--;
+                }
+               
             }
             
-        }
-        static void AccAndBalance()
-        {
+            static void BankMenu()
+            {
+                Console.WriteLine("\n1. Se dina konton och saldo ");
+                Console.WriteLine("2. Överföring mellan konton");
+                Console.WriteLine("3. Ta ut pengar ");
+                Console.WriteLine("4. Logga ut ");
+                Console.Write("\nVälj ett alternativ: ");
 
-        }
-        static void TransferMoney()
-        {
 
-        }
-        static void WithdrawMoney()
-        {
+                try
+                {
+                    int userChoice = Convert.ToInt32(Console.ReadLine());
 
-        }
-        static void LogOut()
-        {
+                    switch (userChoice)
+                    {
+                        case 1:
+                            AccAndBalance();
+                            break;
+                        case 2:
+                            TransferMoney();
+                            break;
+                        case 3:
+                            WithdrawMoney();
+                            break;
+                        case 4:
+                            LogOut();
+                            break;
+                        default:
+                            Console.ForegroundColor = ConsoleColor.Red;
+                            Console.WriteLine("Ogiltigt val. Välj mellan 1-4. ");
+                            Console.ResetColor();
+                            break;
 
+                    }
+
+                }
+                catch
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine("Ogiltigt val. Du måste skriva en siffra mellan 1-4.");
+                    Console.ResetColor();
+                }
+
+                static void AccAndBalance()
+                {
+                    for (int i = 0; i < userAccAndBalance.Length; i++)
+                    {
+                        
+                        Console.Clear();
+                        Console.WriteLine($"Konton: {userAccAndBalance[i]} ");
+                    }
+                }
+                static void TransferMoney()
+                {
+
+                }
+                static void WithdrawMoney()
+                {
+
+                }
+                static void LogOut()
+                {
+                    Console.ForegroundColor= ConsoleColor.Green;
+                    Console.WriteLine("Loggar ut...");
+                    Thread.Sleep(3000);
+                    Console.Clear();
+                    LogIn();
+                }
+            }
         }
     }
 }
